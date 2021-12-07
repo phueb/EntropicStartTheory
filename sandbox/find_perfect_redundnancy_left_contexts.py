b57328ede07d61362027df10187e7824903990c0
phueb <info@philhuebner.com>
2021-12-07 09:38:40 -0600

    improve script to find perfect-redundancy left-contexts

--- sandbox/find_perfect_redundnancy_left_contexts.py
+++ sandbox/find_perfect_redundnancy_left_contexts.py
@@ -1,5 +1,6 @@
 from typing import List, Generator
-from collections import Counter
+from collections import Counter, defaultdict
+import pandas as pd
 
 from aochildes.dataset import ChildesDataSet
 
@@ -8,7 +9,7 @@ from entropicstarttheory import configs
 from entropicstarttheory.bpe import train_bpe_tokenizer
 
 
-POSITION = 1
+POSITION = 3
 NUM_PARTS = 8
 NUM_TYPES = 8000
 
@@ -52,7 +53,6 @@ tokens: List[str] = [t for t in tokenizer.encode(text_special, add_special_token
 # replace special token with original probes
 replaced_probes_it = iter(replaced_probes)
 tokens = [token if token != special_token else next(replaced_probes_it) for token in tokens]
-t2f = Counter(tokens)
 assert len(list(replaced_probes_it)) == 0
 
 
@@ -84,17 +84,22 @@ def gen_pr_left_contexts_by_partition(toks: List[str]) -> Generator[List[str], N
         # yield results only when a whole partition worth of tokens have been processed, then clear results
         if len(tokens_in_part) % num_ts_in_part == 0:
 
+            t2f = Counter(tokens_in_part)  # count only in a partition
+
             # find num perfect-redundancy contexts
             for lc, p in pairs:
-                num = 0
+                lc_f = 0
                 for j, tj in enumerate(tokens_in_part):
                     if tj == lc:
                         if tokens_in_part[j + POSITION] != p:
                             break
                         else:
-                            num += 1
+                            lc_f += 1
                 else:
-                    res.append((lc, p, num))
+
+                    p_f = t2f[p]
+                    prop = lc_f / p_f  # what is the proportion of times a probe is preceded by pr left-context
+                    res.append((lc, p, lc_f, p_f, prop))
 
             yield res
             res = []
@@ -105,12 +110,22 @@ def gen_pr_left_contexts_by_partition(toks: List[str]) -> Generator[List[str], N
 print(f'Looping over {len(tokens)} tokens')
 for part_id, data in enumerate(gen_pr_left_contexts_by_partition(tokens)):
 
+    name2col = defaultdict(list)
     props = []
     for di in data:
-        lc, p, num = di
-        prop = num / t2f[p]  # what is the proportion of times a probe is preceded by pr left-context
-        props.append(prop)
-        print(f'{lc:>12} {p:>12} {num:>6} {t2f[p]:>6} {prop:.12f}')
+        lc_, p_, lc_f_, p_f_, prop_ = di
+        # print(f'{lc_:>12} {p_:>12} {lc_f_:>6} {p_f_:>6} {prop_:.12f}')
+        # collect
+        props.append(prop_)
+        name2col['left-context'].append(lc_)
+        name2col['target'].append(p_)
+        name2col['frequency (left-context)'].append(lc_f_)
+        name2col['frequency (probe)'].append(p_f_)
+        name2col['proportion'].append(prop_)
+        name2col['partition'].append(part_id + 1)
+
+    df = pd.DataFrame(data=name2col)
+    print(df.sort_values(by='proportion').to_latex(index=False))
 
     mean_prop = sum(props) / len(props)
     print(f'Partition {part_id:>6,} | prop={mean_prop} Found {len(data):>6,} perfect-redundancy left-contexts')
